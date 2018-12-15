a0a527668e4718b3aea24021e01fe7291c807719
Tyler Williams <tyler@ogdenstudios.xyz>
2018-12-15 16:30:06 -0700

    add static page content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,19 @@ class PagesController < ApplicationController
 
   def index
   end
+
+  def about
+  end
+
+  def services
+  end
+
+  def process_page
+  end
+
+  def rates
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'books#index'
+  root 'pages#index'
+
+  get '/about', to: "pages#about"
+  get '/services', to: "pages#services"
+  get '/process', to: "pages#process_page"
+  get '/rates', to: "pages#rates"
+  get '/contact', to: "pages#contact"
+
 end
