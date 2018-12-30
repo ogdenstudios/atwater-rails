e86a3f7c936597d7fdd92858b892610a4e856368
Tyler Williams <tyler@ogdenstudios.xyz>
2018-12-29 23:45:29 -0700

    add book/author relationship migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181215182350) do
+ActiveRecord::Schema.define(version: 20181230064238) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20181215182350) do
     t.string "old_filename"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
