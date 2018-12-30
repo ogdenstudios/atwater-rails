b41595470b4ec6466df81a3ee64cfbafbd086951
Tyler Williams <tyler@ogdenstudios.xyz>
2018-12-30 02:07:27 -0700

    add page titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,31 @@
 class PagesController < ApplicationController
 
   def index
+    @title = "The Atwater Group"
   end
 
   def about
+    @title = "The Atwater Group | About"
   end
 
   def services
+    @title = "The Atwater Group | Services"
   end
 
   def process_page
+    @title = "The Atwater Group | Process"
   end
 
   def rates
+    @title = "The Atwater Group | Rates"
   end
 
   def portfolio 
     @authors = Author.all.order('last_name')
+    @title = "The Atwater Group | Portfolio"
   end
 
   def contact
+    @title = "The Atwater Group | Contact"
   end
 end
