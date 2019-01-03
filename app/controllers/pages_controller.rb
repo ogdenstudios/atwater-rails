57a2adba29931ad9ce6d12d82a4627e09890d8bb
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-02 20:30:24 -0700

    pullhome page portfolio to full width

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,30 +3,43 @@ class PagesController < ApplicationController
   def index
     @title = "The Atwater Group"
     @authors = Author.where(featured_author: true)
+    @slug = "home"
   end
 
   def about
     @title = "The Atwater Group | About"
+    @slug = "about"
+
   end
 
   def services
     @title = "The Atwater Group | Services"
+    @slug = "services"
+
   end
 
   def process_page
     @title = "The Atwater Group | Process"
+    @slug = "process"
+
   end
 
   def rates
     @title = "The Atwater Group | Rates"
+    @slug = "rates"
+
   end
 
   def portfolio 
     @authors = Author.all.order('last_name')
     @title = "The Atwater Group | Portfolio"
+    @slug = "portfolio"
+
   end
 
   def contact
     @title = "The Atwater Group | Contact"
+    @slug = "contact"
+
   end
 end
