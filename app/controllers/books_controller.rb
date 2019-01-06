74c0227cbfa0b49160f0d2587bce5d58ef531370
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-05 21:15:57 -0700

    add a hacky method to update to make just one featured book per author

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,6 +42,22 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
+        # HACK: make sure there's only ever one featured book per author
+        if @book.featured == true 
+          author = @book.author
+          if author.books.where(featured: true).count > 1 
+            featured_books = author.books.where(featured: true)
+            book0 = featured_books[0]
+            book1 = featured_books[1]
+            if (book0.updated_at > book1.updated_at)
+              book1.featured = false 
+              book1.save!
+            else 
+              book0.featured = false 
+              book0.save!
+            end
+          end
+        end
         format.html { redirect_to @book, notice: 'Book was successfully updated.' }
         format.json { render :show, status: :ok, location: @book }
       else
@@ -69,6 +85,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:work_done, :genre, :title, :old_filename)
+      params.require(:book).permit(:work_done, :genre, :title, :old_filename, :featured)
     end
 end
