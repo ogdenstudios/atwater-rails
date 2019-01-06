ae585e9ac6606a754187403921dccabb65352dc0
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-05 22:36:21 -0700

    require user authentication before edit, update, destroy on books/authors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   # GET /books
   # GET /books.json
