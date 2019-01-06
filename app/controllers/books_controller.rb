66928d88b093948eb12d7b0af7135633ad8edf18
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-05 21:42:57 -0700

    add image uploader from hartl tutorial, but only up to section 13.62

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -85,6 +85,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:work_done, :genre, :title, :old_filename, :featured)
+      params.require(:book).permit(:work_done, :genre, :title, :old_filename, :featured, :picture)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190104015033) do
+ActiveRecord::Schema.define(version: 20190106043844) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20190104015033) do
     t.datetime "updated_at", null: false
     t.integer "author_id"
     t.boolean "featured"
+    t.string "picture"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
