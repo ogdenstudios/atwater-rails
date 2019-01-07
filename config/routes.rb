087b3a8bc321178ff5f426e9f9a6033543ddebda
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-07 16:28:33 -0700

    authenticate before books/authors resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :authors
-  resources :books
+  authenticate :user do 
+    resources :authors 
+    resources :books
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#index'
