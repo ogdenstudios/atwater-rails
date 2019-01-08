f3ad1ef362451252fee510a579f3ecedf89183a7
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-07 22:20:31 -0700

    fix sidebar scroll issue with js

--- app/assets/javascripts/sidebarToggle.js
+++ app/assets/javascripts/sidebarToggle.js
@@ -1,9 +1,11 @@
-
 function sidebarToggle() {
   var sidebar = document.getElementById("sidebar");
+  var body = document.getElementById("body")
   if (!sidebar.classList.contains("active")) {
     sidebar.classList.add("active");
+    body.style.display = "none";
   } else {
     sidebar.classList.remove("active");
+    body.style.display = "flex";
   }
 }
