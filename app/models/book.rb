f854857bd699b0fe639188869eacd45a3368ee42
Tyler Williams <tyler@ogdenstudios.xyz>
2019-10-27 14:20:58 -0600

    put portfolio back for now

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
     belongs_to :author
     belongs_to :genre 
+    belongs_to :subgenre
     mount_uploader :picture, PictureUploader
 
     enum service: { proof: 0, copyedit: 1 }
