368d7a54cc6065a404c2efd6b932cacc2d3099ce
Tyler Williams <tyler@ogdenstudios.xyz>
2019-10-27 13:38:29 -0600

    add enumerator migration and conversion task to books/service etc

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
     belongs_to :author
     mount_uploader :picture, PictureUploader
+
+    enum service: { proof: 0, copyedit: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306231401) do
+ActiveRecord::Schema.define(version: 2019_10_27_192932) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20190306231401) do
     t.boolean "featured"
     t.string "picture"
     t.string "subgenre"
+    t.integer "service"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
