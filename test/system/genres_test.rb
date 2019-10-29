dcfd8a43eb687a10884c66e02ec0a7be881b698f
Tyler Williams <tyler@ogdenstudios.xyz>
2019-10-29 13:58:15 -0600

    install rspec

--- test/system/genres_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class GenresTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit genres_url
-  #
-  #   assert_selector "h1", text: "Genre"
-  # end
-end
