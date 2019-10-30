0a576d8490415b214acd009df05786fdd50dacdd
Tyler Williams <tyler@ogdenstudios.xyz>
2019-10-30 09:43:12 -0600

    add pending tests to authors controller spec

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -13,5 +13,34 @@ RSpec.describe AuthorsController do
         expect(response).to render_template("index")
       end
     end
+    describe "GET show" do
+        it "assigns authors"
+        it "renders the show template"
+    end
+    describe "GET new" do
+        it "assigns authors"
+        it "authenticates user"
+        it "renders the new template" 
+    end
+    describe "GET edit" do 
+        it "assigns authors"
+        it "authenticates user"
+        it "renders the edit template" 
+    end
+    describe "POST create" do 
+        it "assigns authors"
+        it "authenticates user"
+        it "renders the new template" 
+    end
+    describe "PUT update" do 
+        it "assigns authors"
+        it "authenticates user"
+        it "renders the new template" 
+    end
+    describe "DELETE destroy" do 
+        it "assigns authors"
+        it "authenticates user"
+        it "renders the new template" 
+    end
   end
   
