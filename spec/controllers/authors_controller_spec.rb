207ce0e73a2cef950793a4159ab1e4ae257a2853
Tyler Williams <tyler@ogdenstudios.xyz>
2019-10-30 18:03:49 -0600

    add new template check

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe AuthorsController do
+    include Devise::Test::ControllerHelpers
     describe "GET index" do
       it "assigns @authors" do
         author = create(:author)
         get :index
         expect(assigns[:authors]).to eq([author])
       end
-  
       it "renders the index template" do
         get :index
         expect(response).to render_template("index")
@@ -35,14 +35,35 @@ RSpec.describe AuthorsController do
          end
     end
     describe "GET new" do
-        it "assigns authors"
-        it "authenticates user"
-        it "renders the new template" 
-        it "responds to JSON requests"
+        it "redirects an unauthenticated user" do 
+            get :new 
+            expect(response.status).to eq(302)
+        end
+        it "sets the author" do 
+            user = create(:user)
+            sign_in user
+            get :new 
+            expect(assigns[:author]).to_not eq(nil)
+        end
+        it "renders the new template" do 
+            user = create(:user) 
+            sign_in user
+            get :new
+            expect(response).to render_template("new")
+        end
     end
     describe "GET edit" do 
-        it "assigns authors"
-        it "authenticates user"
+        it "redirects an unauthenticated user" do 
+            get :edit 
+            expect(response.status).to eq(302)
+        end
+        it "assigns authors" do 
+            author = create(:author)
+            user = create(:user)
+            sign_in user
+            get :edit, params: {slug: author.slug}
+            expect(assigns[:author]).to eq(author)
+        end
         it "renders the edit template" 
         it "responds to JSON requests"
     end
